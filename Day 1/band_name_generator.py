3e18e6da57cca6ded033bbecb7d986b380e78f03
JeffreyBoehme <jeffrey.boehme2@gmail.com>
2023-11-18 16:39:21 +1100

    Created Day 1 program and Day 1 AI program

---
+++ Day 1/band_name_generator.py
@@ -0,0 +1,5 @@
+# Generates a bandname based on two inputs from the user
+town = input("What town did you grow up in?\n")
+pet = input("What is the name of your first pet \n")
+
+print(f"Your band name could be {town} {pet} or {pet} {town}")
