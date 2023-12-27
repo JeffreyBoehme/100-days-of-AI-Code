1a898859ed7dac310d31130f8c682db2227512fd
JeffreyBoehme <jeffrey.boehme2@gmail.com>
2023-12-27 16:57:41 +1100

    Updated number_guessing_game

--- Day 12/number_guessing_game.py
+++ Day 12/number_guessing_game.py
@@ -3,38 +3,53 @@ from art import GRAPHIC, TOO_HIGH, TOO_LOW, WINNER, LOSER
 
 print(GRAPHIC)
 
-difficulty = input("Choose a Difficulty. \nType 'hard' or 'easy'. ")
-
 number = random.randint(1, 100)
 
-attempts = 0
-if difficulty == "hard":
-    attempts = 5
-else:
-    attempts = 10
+
+def select_difficulty():
+    difficulty = input("Choose a Difficulty. \nType 'hard' or 'easy'. ")
+    if difficulty == "hard":
+        attempts = 5
+    else:
+        attempts = 10
+    return attempts
 
 
-while attempts > 0:
-    guess = input(
-        "Choose a number between 1 and 100. \nPress enter when you are ready. "
-    )
-    if guess == "":
-        print("You need to enter a number")
-        continue
+def guess():
+    global attempts
+    while True:
+        guess = input(
+            "Choose a number between 1 and 100. \nPress enter when you are ready: "
+        )
+        if guess == "":
+            print("You need to enter a number")
+            continue
+        else:
+            break
     guess = int(guess)
     if guess == number:
         print(WINNER)
         print("You guessed Correctly!")
-        print(f"you still had {attempts} attempts left")
-        break
+        print(f"you still had {(attempts - 1)} attempts left")
+        return True
     elif guess > number:
         print(TOO_HIGH)
-        attempts -= 1
-        print(f"You have {attempts} attempts left")
+        return False
     elif guess < number:
         print(TOO_LOW)
-        attempts -= 1
-        print(f"You have {attempts} attempts left")
+
+        return False
+
+
+attempts = select_difficulty()
+
+
+while attempts > 0:
+    correct = guess()
+    if correct == True:
+        break
+    attempts -= 1
+    print(f"You have {attempts} attempts left")
 
 if attempts == 0:
     print(LOSER)
